2d6f5ecacac04c9be1e91ceca8181096c3bd66fd
Caleb Spare <cespare@gmail.com>
2011-05-10 23:38:59 -0700

    Added some more features.

--- discoball.gemspec
+++ discoball.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = "discoball"
-  s.version = "0.0.1"
+  s.version = "0.0.2"
 
   s.required_rubygems_version = Gem::Requirement.new(">=0") if s.respond_to? :required_rubygems_version=
   s.specification_version = 2 if s.respond_to? :specification_version=
@@ -21,4 +21,5 @@ Gem::Specification.new do |s|
     bin/discoball
   )
   s.add_dependency("colorize", ">=0.5.8")
+  s.add_dependency("trollop", ">=1.16.2")
 end
--- lib/discoball.rb
+++ lib/discoball.rb
@@ -4,29 +4,63 @@ require "colorize"
 
 module Discoball
   UNUSABLE_COLORS = [/black$/, /^default$/, /white$/, /^light/]
+  SINGLE_COLOR = :blue
 
   class Highlighter
-    def initialize(patterns)
+    # Patterns is an array of patterns to match. There are three options that can be changed:
+    #   * If color_mode is :individual, then each unique string matching one of the patterns will be a
+    #       different color.
+    #   * If color_mode is :group_colors, then the matches corresponding to each pattern will be the same color.
+    #   * If color_mode is :one_color, then all matches will be a single color.
+    #   * If match_only is set, then only matching lines will be returned.
+    def initialize(patterns, color_mode = :individual, match_only = false)
       @patterns = patterns
+      @color_mode = color_mode
+      @match_only = match_only
       @color_stack = String.colors.reject { |color| UNUSABLE_COLORS.any? { |unusable| color =~ unusable } }
       @color_assignments = {}
+      if color_mode == :group_colors
+        @patterns.each { |pattern| @color_assignments[pattern] = pop_rotate }
+      end
     end
 
     def filter(line)
-      matches = @patterns.flat_map { |pattern| line.scan(pattern) }.uniq
-      matches.each { |match| line.gsub!(match, highlight(match)) }
-      line
+      match_found = false
+      case @color_mode
+      when :one_color
+        matches = @patterns.flat_map { |pattern| line.scan(pattern) }.uniq
+        match_found ||= !matches.empty?
+        matches.each { |match| highlight!(line, match, SINGLE_COLOR) }
+      when :group_colors
+        @patterns.each do |pattern|
+          matches = line.scan(pattern).uniq
+          match_found ||= !matches.empty?
+          matches.each { |match| highlight!(line, match, @color_assignments[pattern]) }
+        end
+      when :individual
+        matches = @patterns.flat_map { |pattern| line.scan(pattern) }.uniq
+        match_found ||= !matches.empty?
+        matches.each do |match|
+          unless @color_assignments.include? match
+            @color_assignments[match] = pop_rotate
+          end
+          matches.each { |match| highlight!(line, match, @color_assignments[match]) }
+        end
+      end
+      (@match_only && !match_found) ? nil : line
     end
 
     private
 
-    def highlight(match)
-      unless @color_assignments.include? match
-        color = @color_stack.pop
-        @color_stack.insert(0, color)
-        @color_assignments[match] = color
-      end
-      match.colorize(@color_assignments[match]).underline
+    def highlight!(line, match, color)
+      line.gsub!(match, match.colorize(color).underline)
+    end
+
+    # Get the next color and put it at the back
+    def pop_rotate
+      color = @color_stack.pop
+      @color_stack.insert(0, color)
+      color
     end
   end
 end
